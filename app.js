9af19780417ad3c3eecf7b5691ab00ac0f2b452e
jhon mauro <jhmazaja14@gmail.com>
2024-11-07 14:30:29 -0500

    Descripción de los cambios

--- app.js
+++ app.js
@@ -16,13 +16,11 @@ const tipoProyecto = require('./routes/tipoProyecto')
 const etapa = require('./routes/etapa')
 const cliente = require('./routes/cliente')
 const universidad = require('./routes/universidad')
-// const inventario = require('./routes/inventario')
 
 // middlewares
 app.use('/api/tiposproyectos', tipoProyecto)
 app.use('/api/etapas', etapa)
 app.use('/api/clientes', cliente)
 app.use('/api/universidades', universidad)
-// app.use('/api/inventarios', inventario)
 
 module.exports = app
